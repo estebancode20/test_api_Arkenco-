50d00387a0ad5b435516027faf58b7e06ec90a5e
esteban <relbigote75@gmail.com>
2024-05-15 15:35:54 -0400

    add views

--- crud/views.py
+++ crud/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Prospecto
+from .serializer import ProspectoSerializer
 
 # Create your views here.
+
+class ProspectoViewSet(viewsets.ModelsViewset):
+    queryset = Prospecto.objects.all()
+    serializer_class = ProspectoSerializer
+
